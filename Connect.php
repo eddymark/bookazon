bfb0f9787f4e98f1b6f84e53b3c3221ff30952d2
eddy <eddyv5413@gmail.com>
2016-08-03 15:05:51 -0400

    8/3/16

--- Connect.php
+++ Connect.php
@@ -11,28 +11,25 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-$link = mysqli_connect('localhost', 'eddy', 'dtycts16');
-if (!$link)
-{
- $output = 'Unable to connect to the database server.';
- include 'output.html.php';
- exit();
-}
-if (!mysqli_set_charset($link, 'utf8'))
-{
- $output = 'Unable to set database connection encoding.';
- include 'output.html.php';
- exit();
-}
-if (!mysqli_select_db($link, 'eddy'))
-{
- $output = 'Unable to locate the joke database.';
- include 'output.html.php';
- exit();
-}
-$output = 'Database connection established.';
-include 'output.html.php';
-?>
+        $link = mysqli_connect('localhost', 'eddy', 'dtycts16');
+        if (!$link) {
+            $output = 'Unable to connect to the database server.';
+            include 'output.html.php';
+            exit();
+        }
+        if (!mysqli_set_charset($link, 'utf8')) {
+            $output = 'Unable to set database connection encoding.';
+            include 'output.html.php';
+            exit();
+        }
+        if (!mysqli_select_db($link, 'eddy')) {
+            $output = 'Unable to locate the joke database.';
+            include 'output.html.php';
+            exit();
+        }
+        $output = 'Database connection established.';
+        include 'output.html.php';
+        ?>
 
     </body>
 </html>
---
+++ output.html.php
@@ -0,0 +1,10 @@
+<head>
+    <title>PHP Output</title>
+    <meta http-equiv="content-type"
+          content="text/html; charset=utf-8"/>
+</head>
+<body>
+    <p>
+        <?php echo $output; ?>
+    </p>
+</body>
